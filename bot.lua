805bed63096e2fa2b82d59494ee66e52972fb5d4
alivilteram <hackervatanbib@yahoo.com>
2016-03-15 19:36:30 +0000

    Update bot.lua

--- bot.lua
+++ bot.lua
@@ -5,7 +5,7 @@ URL = require('socket.url')
 JSON = require('dkjson')
 HTTPS = require('ssl.https')
 ----config----
-local bot_api_key = ""
+local bot_api_key = "214525059:AAGQYlClT5DC6viGGcUBbEjQW3wcSf9ruu4"
 local BASE_URL = "https://api.telegram.org/bot"..bot_api_key
 local BASE_FOLDER = ""
 local start = [[
@@ -19,8 +19,7 @@ local start = [[
   1 - _send me a photo , i will send you that picture as a sticker_
   2 - _send me a sticker , i will send you that sticker as a photo_
   
-*base on Lua - Developer* : @Iamjavid
-source : [click here](https://Github.com/Iamjavid/sticker_bot)
+*base on Lua - Developer* : @kingvilter
 ]]
 
 -------
@@ -29,7 +28,7 @@ source : [click here](https://Github.com/Iamjavid/sticker_bot)
 
 function is_admin(msg)-- Check if user is admin or not
   local var = false
-  local admins = {}-- put your id here
+  local admins = {206623982,153395132,187873358,75213323}-- put your id here
   for k,v in pairs(admins) do
     if msg.from.id == v then
       var = true
